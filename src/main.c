758956e02af89c1257899232fc2a4c4180a47321
Jack Saysana <saysanabjack@gmail.com>
2022-09-21 21:56:43 -0400

    Fixed movement bug

--- src/main.c
+++ src/main.c
@@ -32,13 +32,13 @@ int main() {
   glViewport(0, 0, 640, 480);
 
   unsigned int shader = init_shader_prog(
-      "/home/jack/projects/OpenGL-Graphics-Engine/src/shaders/phong/shader.vs",
+      "C:/Users/Jack/Documents/C/OpenGL-Graphics-Engine/src/shaders/phong/shader.vs",
       NULL,
-      "/home/jack/projects/OpenGL-Graphics-Engine/src/shaders/phong/shader.fs"
+      "C:/Users/Jack/Documents/C/OpenGL-Graphics-Engine/src/shaders/phong/shader.fs"
       );
 
   MODEL *cube = load_model(
-      "/home/jack/projects/OpenGL-Graphics-Engine/resources/Cube/cube.obj"
+      "C:/Users/Jack/Documents/C/OpenGL-Graphics-Engine/resources/Cube/cube.obj"
       );
 
   mat4 projection = {
@@ -51,12 +51,7 @@ int main() {
   glUniformMatrix4fv(glGetUniformLocation(shader, "projection"), 1,
                      GL_FALSE, (float *)projection);
 
-  mat4 view = {
-    { 1.0, 0.0, 0.0, 0.0 },
-    { 0.0, 1.0, 0.0, 0.0 },
-    { 0.0, 0.0, 1.0, 0.0 },
-    { 0.0, 0.0, 0.0, 1.0 }
-  };
+  mat4 view;
 
   mat4 model = {
     { 1.0, 0.0, 0.0, 0.0 },
@@ -73,15 +68,15 @@ int main() {
     keyboard_input(window);
 
     // Render
-    glm_vec3_add(camera_pos, camera_front, center);
+    glm_vec3_sub(camera_front, camera_pos, center);
     glm_lookat(camera_pos, center, camera_up, view);
 
     //printf("==========\npos: %f %f %f\ncenter: %f %f %f\nup: %f %f %f\n",
     //      camera_pos[0], camera_pos[1], camera_pos[2], center[0], center[1],
     //      center[2], camera_up[0], camera_up[1], camera_up[2]);
-    /*printf("==========\n%f %f %f\n% f %f %f\n%f %f %f\n", view[0][0],
+    printf("==========\n%f %f %f\n% f %f %f\n%f %f %f\n", view[0][0],
            view[0][1], view[0][2], view[1][0], view[1][1], view[1][2],
-           view[2][0], view[2][1], view[2][2]);*/
+           view[2][0], view[2][1], view[2][2]);
     glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1,
                        GL_FALSE, (float *)view);
 
