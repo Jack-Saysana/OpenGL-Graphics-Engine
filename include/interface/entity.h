c917278183080cb625d964b300aa98746430d385
jbs <saysanabjack@gmail.com>
2024-01-26 11:35:31 -0500

    Interface bug fix

--- include/interface/entity.h
+++ include/interface/entity.h
@@ -9,7 +9,8 @@ ENTITY *init_entity(MODEL *model);
 void draw_entity(unsigned int shader, ENTITY *entity);
 void draw_skeleton(unsigned int shader, ENTITY *entity);
 void draw_colliders(unsigned int shader, ENTITY *entity, MODEL *sphere);
-void draw_model(MODEL *mode);
+void draw_model(unsigned int shader, MODEL *model);
+void free_model(MODEL *model);
 void free_entity(ENTITY *entity);
 
 #endif
