d743527dcb93d16f1e814f7d1a2cbfe7d41f95c2
jbs <saysanabjack@gmail.com>
2025-04-10 06:08:55 -0400

    List animations and animation blending

--- include/animation.h
+++ include/animation.h
@@ -6,8 +6,14 @@
 
 // ====================== INTERNALLY DEFINED FUNCTIONS =======================
 
+void apply_animation(ENTITY *entity, ANIMATION animation, unsigned int frame);
+void blend_keyframes(mat4 (*dest)[3], unsigned int bone_id, C_TYPE type,
+                     KEYFRAME *prev, KEYFRAME *next, float ratio);
 void calc_bone_mats(mat4 (*bone_mats)[3], unsigned int bone_id, C_TYPE type,
                     unsigned int frame, KEYFRAME *prev, KEYFRAME *next);
+void apply_blend(ENTITY *entity, ANIMATION a1, ANIMATION a2, unsigned int f1,
+                 unsigned int f2, float ratio);
+void apply_bone_mats(ENTITY *entity);
 
 // ====================== EXTERNALLY DEFINED FUNCTIONS =======================
 
--- include/interface/structs/models/animation_str.h
+++ include/interface/structs/models/animation_str.h
@@ -1,6 +1,26 @@
 #ifndef __ENGINE_ANIMATION_STR_H__
 #define __ENGINE_ANIMATION_STR_H__
 
+#define A_TYPE_SINGLE (0)
+#define A_TYPE_BLEND  (1)
+
+typedef struct animation_input {
+  union {
+    struct {
+      unsigned int index;
+      unsigned int frame;
+    };
+    struct {
+      unsigned int a1;
+      unsigned int a2;
+      unsigned int f1;
+      unsigned int f2;
+      float ratio;
+    };
+  };
+  int type;
+} A_INPUT;
+
 typedef enum chain_type {
   LOCATION = 0,
   ROTATION = 1,
